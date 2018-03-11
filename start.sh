d2e6c1b73cc67fe09625db30cc6a408d3720f483
leoni <leonimega@hotmail.com>
2018-03-11 16:48:47 -0300

    update start script

--- start.sh
+++ start.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
-#
 # Copyright IBM Corp All Rights Reserved
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 # Exit on first error, print all commands.
 set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
-docker-compose -f docker-compose.yml down
+# orgs declaration
+declare -a ORGS_LIST=("supplier1" "supplier2" "city1")
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml up -d
+
+# without CLI:
+# docker-compose -f docker-compose.yml up -d \
+# orderer.leonimurilo.com \
+# peer0.supplier1.leonimurilo.com \
+# ca.supplier1.leonimurilo.com \
+# couchdb.supplier1.leonimurilo.com \
+# peer0.supplier2.leonimurilo.com \
+# ca.supplier2.leonimurilo.com \
+# couchdb.supplier2.leonimurilo.com \
+# peer0.city1.leonimurilo.com \
+# ca.city1.leonimurilo.com \
+# couchdb.city1.leonimurilo.com
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=10
-#echo ${FABRIC_START_TIMEOUT}
+export FABRIC_START_TIMEOUT=15
+echo Waiting ${FABRIC_START_TIMEOUT} seconds
 sleep ${FABRIC_START_TIMEOUT}
 
-# Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mi-flow-channel -f /etc/hyperledger/configtx/channel.tx
-# Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mi-flow-channel.block
+for ORG in "${ORGS_LIST[@]}"
+do
+  echo
+  echo "========= Creating channel on ${Purple}peer0.$ORG.leonimurilo.com${NC}"
+  ORG_MSP="$(tr '[:lower:]' '[:upper:]' <<< ${ORG:0:1})${ORG:1}MSP"
+  # Create the channel
+  docker exec \
+  -e "CORE_PEER_LOCALMSPID=$ORG_MSP" \
+  -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@$ORG.leonimurilo.com/msp" \
+  peer0.$ORG.leonimurilo.com peer channel create \
+  -o orderer.leonimurilo.com:7050 \
+  -c mi-flow-channel \
+  -f /etc/hyperledger/configtx/channel.tx
+  echo "========= ${Cyan}Done!${NC}"
+
+  echo
+  echo "========= Joining channel on ${Purple}peer0.$ORG.leonimurilo.com${NC}"
+  # Join peer0.$ORG.leonimurilo.com to the channel.
+  docker exec \
+  -e "CORE_PEER_LOCALMSPID=$ORG_MSP" \
+  -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@$ORG.leonimurilo.com/msp" \
+  peer0.$ORG.leonimurilo.com peer channel join \
+  -b mi-flow-channel.block
+  echo "========= ${Cyan}Done!${NC}"
+  echo
+  echo
+done
+
+exit 0
