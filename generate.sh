feddde624eb33929322167615d3e364a1fd2388a
leoni <leonimega@hotmail.com>
2018-03-11 12:58:17 -0300

    update generate.sh and configtx file

--- generate.sh
+++ generate.sh
@@ -7,6 +7,8 @@
 export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 CHANNEL_NAME=mi-flow-channel
+GENESIS_PROFILE=ThreeOrgsOrdererGenesis
+CHANNEL_PROFILE=ThreeOrgsChannel
 
 # remove previous crypto material and config transactions
 rm -fr config/configtx/*
@@ -20,21 +22,21 @@ if [ "$?" -ne 0 ]; then
 fi
 
 # generate genesis block for orderer
-configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+configtxgen -profile $GENESIS_PROFILE -outputBlock ./config/configtx/genesis.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile $CHANNEL_PROFILE -outputCreateChannelTx ./config/configtx/channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile $CHANNEL_PROFILE -outputAnchorPeersUpdate ./config/configtx/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org1MSP..."
   exit 1
